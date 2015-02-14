d503e1435829c171757e7971bba914bfd4525566
mmk2410 <marcelmichaelkapfer@yahoo.co.nz>
2015-02-14 17:45:23 +0100

    Added the other sections

--- app/src/main/java/de/marcelkapfer/morseconverter/MainActivity.java
+++ app/src/main/java/de/marcelkapfer/morseconverter/MainActivity.java
@@ -7,13 +7,18 @@ import it.neokree.materialnavigationdrawer.elements.MaterialSection;
 
 public class MainActivity extends MaterialNavigationDrawer {
 
-    private MaterialSection writtenMorse;
+    private MaterialSection writtenMorse, normalMorse, about;
 
     @Override
     public void init(Bundle savedInstanceState) {
         writtenMorse = this.newSection("writtenMorse", new MainFragment());
+        normalMorse = this.newSection("Normal Morse", new MorseFragment());
+        about = this.newSection("About", new AboutFragment());
         this.addSection(writtenMorse);
+        this.addSection(normalMorse);
+        this.addBottomSection(about);
         this.setDrawerHeaderImage(this.getResources().getDrawable(R.drawable.feature_graphics));
         allowArrowAnimation();
+        this.disableLearningPattern();
     }
 }
